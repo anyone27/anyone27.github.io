8c548c1918c3120a37dd3f3a45cdb43bada467df
Josh Callarman <joshcalluk@gmail.com>
2021-10-07 14:25:13 +0100

    global dark mode

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const btn = document.querySelector(".switch");
 
-btn.addEventListener("change", function () {
-  document.body.classList.toggle("dark-theme");
-});
+document.onload = checkStyle();
+
+btn.addEventListener("change", darkMode);
+
+function checkStyle() {
+    let checkDarkMode = localStorage.getItem("darkMode")
+    if (checkDarkMode != "true") {
+        localStorage.setItem("darkMode", "false");
+        document.body.classList.remove("dark-theme");
+    } else {
+        document.body.classList.add("dark-theme");
+    }
+}
+
+function darkMode() {
+    if ((localStorage.getItem("darkMode")) == "false") {
+        localStorage.setItem('darkMode', 'true');
+        document.body.classList.add("dark-theme");
+    } else if ((localStorage.getItem("darkMode")) === "true") {
+        localStorage.setItem('darkMode', 'false');
+        document.body.classList.remove("dark-theme");
+    }
+}
