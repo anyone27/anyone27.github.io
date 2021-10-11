a6be3b34fe9a3ed23ec6349c189bc371a244cb22
Josh Callarman <joshcalluk@gmail.com>
2021-10-11 14:55:46 +0100

    checkbox honors on/off style

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const btn = document.querySelector(".switch");
+const slideLabel = document.getElementById("sliderLabel");
+const slideCheck = document.getElementById("sliderCheck");
 
 document.onload = checkStyle();
 
@@ -9,8 +11,13 @@ function checkStyle() {
     if (checkDarkMode != "true") {
         localStorage.setItem("darkMode", "false");
         document.body.classList.remove("dark-theme");
+        slideLabel.innerHTML = "Dark Mode"
+        slideCheck.checked = true;
     } else {
         document.body.classList.add("dark-theme");
+        slideLabel.innerHTML = "Light Mode"
+        slideCheck.checked = false;
+
     }
 }
 
@@ -18,8 +25,12 @@ function darkMode() {
     if ((localStorage.getItem("darkMode")) == "false") {
         localStorage.setItem('darkMode', 'true');
         document.body.classList.add("dark-theme");
+        slideLabel.innerHTML = "Light Mode"
+        slideCheck.checked = false;
     } else if ((localStorage.getItem("darkMode")) === "true") {
         localStorage.setItem('darkMode', 'false');
         document.body.classList.remove("dark-theme");
+        slideLabel.innerHTML = "Dark Mode"
+        slideCheck.checked = true;
     }
 }
